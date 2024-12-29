31c3973bfa5c5301cf5f1ca2bde5f66358ed08fc
IteratorInnovator <harryngkokjing@gmail.com>
2024-12-29 19:40:26 +0800

    Define send_email function. Update .env file Add harryngkokjing@gmail.com Restructure main.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,6 @@
 import json 
 import smtplib
+import ssl
 from datetime import datetime, timedelta
 from email.message import EmailMessage
 
@@ -23,40 +24,71 @@ def get_tomorrow_day():
 def get_time():
     return datetime.today().time()
     
-def send_email(event,sender_email,recipient_email):
-    event_name = event["name"]
-    location = event["location"]
-    time = event["time"]
-    email_content = f"""
-    Hi Harry,
-    
-    This is a reminder that you have the following class tomorrow:
-    
-    Class: {event_name}
-    Location: {location}
-    Time: {time}
-
-    Please ensure you're prepared for the session.
-
-    Best regards,
-    Automated Reminder System
-    """
+def send_email(event,sender_email,recipient_emails,app_password,email_contents):
     msg = EmailMessage()
-    msg["Subject"] = f"REMINDER - {event_name}"
+    msg["Subject"] = f"REMINDER - {event["name"]}"
     msg["From"] = sender_email
-    msg["To"] = recipient_email
-    msg.set_content(email_content)
-
+    msg["To"] = ','.join(recipient_emails)
+    msg.set_content(email_contents[0])
+    msg.add_alternative(email_contents[1],subtype="html")
+    
+    context = ssl.create_default_context()
+    # SSL connections: port 465
+    # If using starttls(), use port 587
+    # Close using server.close() if no 'with' statement used
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as server:
+            server.login(sender_email,app_password)
+            server.sendmail(sender_email,recipient_emails,msg.as_string())
+            # Convert EmailMessage object into a properly formatted string that follows the MIME standard
+    except Exception as e:
+        print(f"Error: {e}")
+        
+        
+        
 # For recurring events, retrieve day of tomorrow and date
-# Process event and craft subject and body
-# If more or equal than, send email
+# Process event and craft email body
+# Return email content for plain text and html
 def email_alerts_recurring(event,day_of_tomorrow,current_time):
     # Check if day of tomorrow in the list of event days
     if day_of_tomorrow in event["day"]:
         event_time = datetime.strptime(event["time"],"%I:%M %p").time()
         # Check if current time in within 24 hr mark
         if event_time < current_time:
-            pass
+            event_name = event["name"]
+            location = event["location"]
+            time = event["time"]
+            email_content = f"""
+Hi Harry,
     
+This is a reminder that you have the following class tomorrow:
+    
+Class: {event_name}
+Location: {location}
+Time: {time}
+
+Please ensure you are prepared for the lesson.
+
+Best regards,
+Automated Reminder System
+            """
+            email_content_in_html = f"""
+    <html>
+        <body>
+            <h1 style="color: #0056b3;"><b>REMINDER</b></h1>
+            <p>Hi Harry,</p>
+            <p>This is a reminder that you have the following class <b>tomorrow</b>:</p>
+            <p>
+                <b>Class:</b> {event_name}<br>
+                <b>Location:</b> {location}<br>
+                <b>Time:</b> {time}<br><br>
+            </p>
+            <p>Please ensure you are prepared for the lesson.</p>
+            <p>Best regards,<br>Automated Reminder System</p>
+        </body>
+    </html>
+            """
+            return (email_content,email_content_in_html)
+    return ()
 def email_alerts_non_recurring(event,date_of_tomorrow,current_time):
     pass
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     load_dotenv()
     sender_email = os.getenv("SENDER_EMAIL")
     app_password = os.getenv("APP_PASSWORD")
-    recipient_email = os.getenv("RECIPIENT_EMAIL")
+    recipient_emails = os.getenv("RECIPIENT_EMAIL").split(',')
     events = HF.load_events(JSON_FILE)
     day_of_tomorrow = HF.get_tomorrow_day()
     date_of_tomorrow = HF.get_tomorrow_date()
@@ -16,12 +16,18 @@ def main():
     for event in events:
         if event["reminder_sent"] == False:
             if event["recurring"] == True:
-                HF.email_alerts_recurring(event,day_of_tomorrow,current_time)
+                email_contents = HF.email_alerts_recurring(event,day_of_tomorrow,current_time)
+                if len(email_contents)==2:
+                    HF.send_email(event,sender_email,recipient_emails,app_password,email_contents)
+                continue # skip to next iteration if email_contents size is incorrect, indicating not to send email
             elif event["recurring"] == False:
-                HF.email_alerts_non_recurring(event,date_of_tomorrow,current_time)
-        elif event["remidner_sent"] == True:
+                email_contents = HF.email_alerts_non_recurring(event,date_of_tomorrow,current_time)
+                if len(email_contents)==2:
+                    HF.send_email(event,sender_email,recipient_emails,app_password,email_contents)
+                continue
+        elif event["reminder_sent"] == True:
             if event["recurring"] == True:
-                    pass
+                pass
     
 if __name__ == "__main__":
     main()
