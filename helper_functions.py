ec87f189c1c192a8fe0c226b1a95a1df537f026e
IteratorInnovator <harryngkokjing@gmail.com>
2024-12-29 18:16:23 +0800

    Update scripts

--- helper_functions.py
+++ helper_functions.py
@@ -11,7 +11,7 @@ def load_events(JSON_FILE):
 # Get tomorrow's date in YYYY-MM-DD format
 def get_tomorrow_date():
     tomorrow_date = datetime.today() + timedelta(days=1)
-    return tomorrow_date.strftime('%Y-%m-%d')
+    return tomorrow_date.date()
 
 # Get the day in abbreaviated name for tomorrow 
 def get_tomorrow_day():
@@ -23,11 +23,40 @@ def get_tomorrow_day():
 def get_time():
     return datetime.today().time()
     
-def send_email():
-    pass
+def send_email(event,sender_email,recipient_email):
+    event_name = event["name"]
+    location = event["location"]
+    time = event["time"]
+    email_content = f"""
+    Hi Harry,
+    
+    This is a reminder that you have the following class tomorrow:
+    
+    Class: {event_name}
+    Location: {location}
+    Time: {time}
 
-def email_alerts_recurring():
-    pass
+    Please ensure you're prepared for the session.
 
-def email_alerts_non_recurring():
+    Best regards,
+    Automated Reminder System
+    """
+    msg = EmailMessage()
+    msg["Subject"] = f"REMINDER - {event_name}"
+    msg["From"] = sender_email
+    msg["To"] = recipient_email
+    msg.set_content(email_content)
+
+# For recurring events, retrieve day of tomorrow and date
+# Process event and craft subject and body
+# If more or equal than, send email
+def email_alerts_recurring(event,day_of_tomorrow,current_time):
+    # Check if day of tomorrow in the list of event days
+    if day_of_tomorrow in event["day"]:
+        event_time = datetime.strptime(event["time"],"%I:%M %p").time()
+        # Check if current time in within 24 hr mark
+        if event_time < current_time:
+            pass
+    
+def email_alerts_non_recurring(event,date_of_tomorrow,current_time):
     pass
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import json
-import email_alerts
+import helper_functions as HF
 from dotenv import load_dotenv
 
 JSON_FILE = "./events.json"
@@ -9,14 +9,19 @@ def main():
     sender_email = os.getenv("SENDER_EMAIL")
     app_password = os.getenv("APP_PASSWORD")
     recipient_email = os.getenv("RECIPIENT_EMAIL")
-    events = email_alerts.load_events(JSON_FILE)
+    events = HF.load_events(JSON_FILE)
+    day_of_tomorrow = HF.get_tomorrow_day()
+    date_of_tomorrow = HF.get_tomorrow_date()
+    current_time = HF.get_time()
     for event in events:
         if event["reminder_sent"] == False:
             if event["recurring"] == True:
-                email_alerts.email_alerts_recurring(event)
+                HF.email_alerts_recurring(event,day_of_tomorrow,current_time)
             elif event["recurring"] == False:
-                email_alerts.email_alerts_non_recurring(event)
-            
+                HF.email_alerts_non_recurring(event,date_of_tomorrow,current_time)
+        elif event["remidner_sent"] == True:
+            if event["recurring"] == True:
+                    pass
     
 if __name__ == "__main__":
     main()
